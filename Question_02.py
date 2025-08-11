52437d1a47a92b970be52b92150c27e8eb0a4372
Savidilsh <savindudilshan124@gmail.com>
2025-08-12 00:07:40 +0530

    done

--- Question_02.py
+++ Question_02.py
@@ -7,49 +7,45 @@ img_orig = cv.imread('C:/Users/Savindu Dilshan/Desktop/Github/Intensity-Transfor
 
 # Define intensity transformation for white matter 
 lut_white = np.zeros(256, dtype=np.uint8)
-for i in range(256):
-    if i < 100:
-        lut_white[i] = int(i * 255 / 100) 
-    else:
-        lut_white[i] = 255  
+lut_white[0:175] = np.linspace(255, 255, 175)
+lut_white[175:200] = np.linspace(255, 0, 25)
+lut_white[200:] = 0
+
+# Apply transformation for gray matter
+lut_gray = np.zeros(256, dtype=np.uint8)
+lut_gray[0:50] = 255
+lut_gray[50:125] = np.linspace(255, 0, 75)
+lut_gray[125:200] = np.linspace(0, 255, 75)
+lut_gray[200:] = 255
 
 white_img = cv.LUT(img_orig, lut_white)
-
-# Define intensity transformation for gray matter 
-lut_gray = np.zeros(256, dtype=np.uint8)
-for i in range(256):
-    if 100 <= i < 200:
-        lut_gray[i] = int((i - 100) * 255 / 100)  
-    else:
-        lut_gray[i] = 0  
-
 gray_img = cv.LUT(img_orig, lut_gray)
 
 # Display results using subplots
 plt.figure(figsize=(20, 4))
 
-plt.subplot(161)
+plt.subplot(151)
 plt.imshow(img_orig, cmap='gray')
 plt.title('Original Image')
 plt.axis('off')
 
-plt.subplot(162)
+plt.subplot(152)
 plt.imshow(white_img, cmap='gray')
 plt.title('White Matter Accentuated')
 plt.axis('off')
 
-plt.subplot(163)
+plt.subplot(153)
 plt.imshow(gray_img, cmap='gray')
 plt.title('Gray Matter Accentuated')
 plt.axis('off')
 
-plt.subplot(164)
+plt.subplot(154)
 plt.plot(lut_white)
 plt.title('White Matter Transformation')
 plt.xlabel('Input Intensity')
 plt.ylabel('Output Intensity')
 
-plt.subplot(165)
+plt.subplot(155)
 plt.plot(lut_gray)
 plt.title('Gray Matter Transformation')
 plt.xlabel('Input Intensity')
